80464ec856dfcd37e64b42b25d7ff65814de3ad8
퍼드 <taehyeonine@gmail.com>
2025-08-14 08:40:33 +0900

    feat:add-test

--- iphoneMemo/Test/FeatureCard.swift
+++ iphoneMemo/Test/FeatureCard.swift
@@ -17,15 +17,20 @@ struct FeatureCard: View {
                 .font(.largeTitle)
                 .frame(width: 50)
                 .padding(.trailing, 10)
-            
+
             Text(description)
 
-            //            FeatureCard(iconName: "person.2.crop.square.stack.fill", description: "A multiline description about a feature paired with the image on the left.")
             Spacer()
 
         }
         .padding()
-        .background(.tint, in: RoundedRectangle(cornerRadius: 12))
+        .background {
+            RoundedRectangle(cornerRadius: 12)
+                .foregroundStyle(.tint)
+                .opacity(0.25)
+                .brightness(-0.4)
+
+        }
         .foregroundStyle(.white)
     }
 }
